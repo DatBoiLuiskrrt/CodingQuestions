f821306c2fee080c66827706993b02cb03de9cc7
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-07-13 01:06:39 -0500

    naming conventions in js

---
+++ Javascript/naming.js
@@ -0,0 +1,4 @@
+// constant except arrays and objects should be capitalized
+//should be able to pass the parent/significant other test
+//basically name every variable in a explicit way
+//use camelcase
