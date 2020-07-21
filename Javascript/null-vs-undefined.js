f1d56892a3b5dad288deadd6b9601c5f2ed86fb0
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-07-21 04:01:02 -0500

    null vs undefined

--- Javascript/null-vs-undefined.js
+++ Javascript/null-vs-undefined.js
@@ -1,2 +1,9 @@
 //undefined means that a variable has not been defined a value
 //null an assigned value. this has yet to have a value and I assigned it the value of null
+
+const user = {
+
+};
+let user1 = null;
+console.log(user);
+console.log(user.firstName);// undefined
