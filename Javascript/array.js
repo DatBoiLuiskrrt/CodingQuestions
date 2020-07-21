991e4834af39a3441a6707f0985d3c589ca2799d
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-07-21 03:46:38 -0500

    how to create arrays

--- Javascript/array.js
+++ Javascript/array.js
@@ -1,2 +1,8 @@
 // how to create an array
 const example = [];
+//by mapping as well filter or reduce
+const example2 = example1.map((val) => {
+    return val + 1;
+});
+console.log(example1);
+console.log(example2);
