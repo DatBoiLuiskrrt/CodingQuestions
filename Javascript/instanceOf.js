fdad4265226577d11fccabafb8405751f1190f79
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-07-22 02:16:18 -0500

    what is an instance of

---
+++ Javascript/instanceOf.js
@@ -0,0 +1,6 @@
+console.log("hello world" instanceof String);
+
+class Circle{};
+const circle = new Circle();
+console.log( circle instanceof Circle);
+//this returns true beacuse its an instnce of a class 
