8586a10423b1171bdbf2c8d0cb14dae7fe26f592
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-07-25 02:53:48 -0500

    how to destructure arrays and objects

--- Javascript/destructure.js
+++ Javascript/destructure.js
@@ -1,9 +1,13 @@
 const birthday = [01, 10, 1996];
+//be more descriptive
+const [month, day, year] = [01, 10, 1996];
+console.log(`${month}/${day}/${year}`);
 
-const user = {
+const {firstName: fn, nickName} = {
     firstName: 'Luis',
     lastName: 'Hernandez',
     nickName: 'Minetito'
 };
 
-console.log();
+console.log(fn);
+console.log(nickName);
